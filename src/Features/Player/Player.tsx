947b6e1ecaec96ac4af44139c64abc7c3f5bd06e
saharsoffer <sahars@tencyle.com>
2023-11-23 08:34:41 +0200

    move thumbnail gathering logic to its own hook

--- src/Features/Player/Player.tsx
+++ src/Features/Player/Player.tsx
@@ -1,88 +1,25 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { AspectRatio, Thumbnail } from "../../type";
 import styles from "./Player.module.scss";
 import classNames from "classnames";
-import { getRatioHeight } from "../../utils";
-
-function wait() {
-  return new Promise<unknown>((resolve: (value: unknown) => void) => {
-    setTimeout(() => {
-      resolve(null);
-    }, 36);
-  });
-}
+import { getRatioHeight, wait } from "../../utils";
+import { blackImage } from "../../const";
 
 interface PlayerProps {
   src: string;
-  onThumbnailGenerated: (thumbnail: Thumbnail[]) => void;
   aspectRatio: AspectRatio;
-  onDone: (done: boolean) => void;
   onLoad: (video: HTMLVideoElement) => void;
 }
 
-export default function Player({
-  src,
-  onThumbnailGenerated,
-  aspectRatio,
-  onDone,
-  onLoad,
-}: PlayerProps) {
-  const [done, setDone] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  useEffect(() => {
-    const video = videoRef.current;
-    const canvas = document.createElement("canvas");
-    if (!video || !canvas) return;
-    const ctx = canvas.getContext("2d");
-
-    const thumbnails: Thumbnail[] = [];
-    let startTime = 0;
-    const frameSkipMs = 10;
-
-    video.addEventListener("loadedmetadata", async function () {
-      onLoad(video);
-
-      for (let i = 1; i <= video.duration; i++) {
-        if (i % frameSkipMs === 0) {
-          await wait();
-          video.currentTime = i;
-          captureThumbnail(video.currentTime);
-        }
-      }
-      setDone(true);
-      onThumbnailGenerated && onThumbnailGenerated(thumbnails);
-    });
-    video.addEventListener("timeupdate", function () {});
-
-    function captureThumbnail(time: number) {
-      console.log(video.currentTime);
-      canvas.width = video.width;
-      canvas.height = video.height;
-      ctx.drawImage(
-        video,
-        0,
-        0,
-        canvas.width,
-        getRatioHeight(canvas.width, aspectRatio),
-      );
-
-      thumbnails.push({
-        time: time as number,
-        src: canvas.toDataURL("image/jpeg"),
-      });
-      console.log(thumbnails);
-    }
-  }, []);
+export default function Player({ src, aspectRatio, onLoad }: PlayerProps) {
   return (
     <div>
-      {!done && <div className={styles.loading}>loading...</div>}
       <video
-        width="640"
-        height="360"
+        width={aspectRatio[0]}
+        height={aspectRatio[1]}
         controls
-        ref={videoRef}
         style={{ display: "block" }}
-        className={classNames(styles.Player, { [styles.loading]: !done })}
+        className={classNames(styles.Player)}
       >
         <source src={src} type="video/mp4" />
         Your browser does not support the video tag.
---
+++ src/hooks/useGetThumbnails.ts
@@ -0,0 +1,69 @@
+import { useEffect, useMemo, useState } from "react";
+import { AspectRatio, Thumbnail } from "../type";
+import { getRatioHeight, wait } from "../utils";
+import { blackImage } from "../const";
+
+interface GetThumbnailsProps {
+  src: string;
+  aspectRatio: AspectRatio;
+}
+
+export default function useGetThumbnails({
+  src,
+  aspectRatio,
+}: GetThumbnailsProps) {
+  const [thumbnails, setThumbnails] = useState<Thumbnail[]>();
+  const tempVideo = useMemo(() => {
+    const video = document.createElement("video");
+    video.innerHTML = `<source src="${src}" type="video/mp4" />`;
+    video.width = aspectRatio[0];
+    video.height = aspectRatio[1];
+    return video;
+  }, [aspectRatio]);
+
+  useEffect(() => {
+    if (![tempVideo, aspectRatio].some(Boolean)) return;
+    const video = tempVideo;
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const thumbnails: Thumbnail[] = [];
+    const frameSkipMs = 10;
+
+    video.addEventListener("loadedmetadata", async function () {
+      for (let i = 1; i <= video.duration; i++) {
+        if (i % frameSkipMs === 0) {
+          video.currentTime = i;
+          await wait();
+          captureThumbnail(video.currentTime);
+        }
+      }
+      video.currentTime = video.duration;
+      await wait();
+      captureThumbnail(video.currentTime);
+
+      setThumbnails(thumbnails);
+    });
+
+    function captureThumbnail(time: number) {
+      canvas.width = video.width;
+      canvas.height = video.height;
+      ctx.drawImage(
+        video,
+        0,
+        0,
+        canvas.width,
+        getRatioHeight(canvas.width, aspectRatio),
+      );
+      const src = canvas.toDataURL("image/jpeg");
+
+      if (!src.includes(blackImage)) {
+        thumbnails.push({
+          time: time as number,
+          src,
+        });
+      }
+    }
+  }, [tempVideo, aspectRatio]);
+
+  return { thumbnails };
+}
