d68e01692396fd41b09d5e2f46c3825500f88077
saharsoffer <sahars@tencyle.com>
2023-11-23 09:56:25 +0200

    implement app composition in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from 'react';
-import './index.scss';
+import React, { useCallback, useState } from "react";
+import "./index.scss";
+import videoSrc from "./assets/sample.mp4";
+import { AspectRatio, Thumbnail } from "./type";
+import { Timeline, Player } from "./Features";
+import { useGetThumbnails, useVideoControl } from "./hooks";
+import PlayButton from "./Features/PlayButton/PlayButton";
+const aspectRatio: AspectRatio = [640, 360];
 
 function App() {
+  const [video, setVideo] = useState<HTMLVideoElement | null>(null);
+  const { thumbnails, duration } = useGetThumbnails({
+    src: videoSrc,
+    aspectRatio,
+  });
+  const { onClipStart, onClipEnd, onPlay, pause } = useVideoControl({ video });
   return (
     <div className="App">
-
+      <Player src={videoSrc} aspectRatio={aspectRatio} onLoad={setVideo} />
+      <div className={"layout-flex-y gap-3"}>
+        <PlayButton onClick={onPlay} pause={pause} />
+        <div className={"layout-spread-x"}>
+          <Timeline
+            onClipStart={onClipStart}
+            onClipEnd={onClipEnd}
+            thumbnails={thumbnails}
+            aspectRatio={aspectRatio}
+            duration={duration}
+          />
+        </div>
+      </div>
     </div>
   );
 }
