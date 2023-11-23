a939d5ac2a68df69fbdaef6a097cdbc98618bd47
saharsoffer <sahars@tencyle.com>
2023-11-23 08:40:23 +0200

    Timeline: deprecate the "done" prop

--- src/Features/Timeline/Timeline.tsx
+++ src/Features/Timeline/Timeline.tsx
@@ -5,20 +5,19 @@ import { useState } from "react";
 interface TimelineProps {
   thumbnails: Thumbnail[];
   aspectRatio: AspectRatio;
-  done: boolean;
   duration: number;
 }
 
-export default function Timeline({
-  thumbnails,
-  done,
-  duration,
-}: TimelineProps) {
+export default function Timeline({ thumbnails, duration }: TimelineProps) {
   const [currentTime, setCurrentTime] = useState(0);
-  console.log(duration);
+
   return (
-    <TimeBar currentTime={currentTime} done={done} duration={duration}>
-      {thumbnails.map((thumbnail, index) => (
+    <TimeBar
+      currentTime={currentTime}
+      done={Boolean(thumbnails)}
+      duration={duration}
+    >
+      {thumbnails?.map((thumbnail, index) => (
         <div
           key={thumbnail.time}
           style={{ backgroundImage: `url(${thumbnail.src})` }}
