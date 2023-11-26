732f2ee49eb93f3b97f8ed0bf9181a04995dab39
saharsoffer <sahars@tencyle.com>
2023-11-26 08:08:48 +0200

    ProgressBar: format time according to new standard (getFormattedTime)

--- src/Features/ProgressBar/ProgressBar.tsx
+++ src/Features/ProgressBar/ProgressBar.tsx
@@ -1,10 +1,15 @@
 import React, { useMemo } from "react";
+import getFormattedTime from "../../utils/getFormattedTime";
 
 interface ProgressBarProps {
   onChange: (value: number) => void;
   currentTime: number;
   clipRange: [number, number];
   clippedDuration: number;
+}
+
+function getFixed(num: number) {
+  return Number(num.toFixed(5));
 }
 
 export default function ProgressBar({
@@ -16,9 +21,9 @@ export default function ProgressBar({
   const startAt = clipRange[0];
   const displayRange = useMemo(() => {
     if (!clippedDuration) return "";
-    return `${Math.ceil(currentTime - clipRange[0])} / ${Math.ceil(
-      clippedDuration,
-    )}`;
+    return `${getFormattedTime(
+      getFixed(currentTime) - getFixed(clipRange[0]),
+    )} / ${getFormattedTime(clippedDuration)}`;
   }, [clippedDuration, clipRange, currentTime]);
 
   return (
@@ -29,7 +34,7 @@ export default function ProgressBar({
       <input
         type="range"
         min={0}
-        max={clippedDuration}
+        max={clippedDuration && Math.floor(clippedDuration)}
         step="0.01"
         className={"width-full"}
         onChange={(e) => {
